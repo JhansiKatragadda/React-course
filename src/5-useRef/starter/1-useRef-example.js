d7c45ec982c37e5fcf3258ddee1ce73fc04477ff
JhansiKatragadda <jhansikatragadda5@gmail.com>
2024-05-21 13:03:21 +0530

    useref-1

--- src/5-useRef/starter/1-useRef-example.js
+++ src/5-useRef/starter/1-useRef-example.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 
-const Starter = () => {
-  return <div>Starter</div>;
+const Final = () => {
+  const [firstName, setFirstName] = useState("");
+  const renderCount = useRef(1);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+
+  return (
+    <>
+      <input
+        type="text"
+        name="firstName"
+        id="firstName"
+        onChange={(e) => {
+          setFirstName(e.target.value);
+        }}
+      />
+      <h5>Typing: {firstName}</h5>
+      <h5>Component renderd {renderCount.current} times</h5>
+    </>
+  );
 };
 
-export default Starter;
+export default Final;
